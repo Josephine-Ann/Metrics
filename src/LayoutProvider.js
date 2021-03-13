e391e62e9b3cc77c21e71098c7df5a13c1abf45a
Josephine-Ann <josephineanngeoghegan@gmail.com>
2021-03-13 17:04:17 +0100

    data now taken locally

--- src/LayoutProvider.js
+++ src/LayoutProvider.js
@@ -1,5 +1,5 @@
-import React from 'react'
-
+import React from 'react';
+import metricsData from './csv/metrics.json';
 
 export const LayoutContext = React.createContext();
 
@@ -23,7 +23,7 @@ export class LayoutProvider extends React.Component {
             data: [],
             COLORS: ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', "rgb(136, 132, 216)"],
             RADIAN: Math.PI / 180,
-            areaData: []
+            areaData: [],
         };
     }
 
@@ -52,118 +52,114 @@ export class LayoutProvider extends React.Component {
         let index = 0
         let connections = []
         let disconnections = []
-        fetch('http://localhost:4000/getAllMetrics')
-            .then(res => res.json())
-            .then(json => {
-                json.map((item) => {
-                    scatter.push({ x: item.connected_time, y: item.total_messages, z: index })
-                    totalClients += 1
-                    if (!clientsInCountries[0].hasOwnProperty(item.country.toLowerCase())) {
-                        clientsInCountries[0][item.country.toLowerCase()] = 1
-                    } else {
-                        clientsInCountries[0][item.country.toLowerCase()] += 1
-                    }
-                    client = item.client.toLowerCase()
-                    if (figures.connections[client]) {
-                        figures.connections[client] += item.connections
-                        figures.disconnections[client] += item.disconnections
-                        figures.time[client] += item.connected_time
-                        figures.averageLatency[client] += item.latency
-                        figures.beaconBlocks[client] += item.beacon_blocks
-                        figures.quantityClients[client] += 1
-                        figures.beaconBlocksAggregations[client] += item.beacon_aggregations
-                    } else {
-                        this.setState({
-                            allClients: item.client
-                        })
-                        figures.connections[client] = item.connections || 0
-                        figures.disconnections[client] = item.disconnections || 0
-                        figures.time[client] = item.connected_time || 0
-                        figures.averageLatency[client] = item.averageLatency || 0
-                        figures.beaconBlocks[client] = item.beacon_blocks || 0
-                        figures.quantityClients[client] = 1
-                        figures.beaconBlocksAggregations[client] = item.beacon_aggregations || 0
-                    }
-                    index++
-                })
-                let country = ""
-                for (const property in clientsInCountries[0]) {
-                    country = property[0].toUpperCase() + property.slice(1, property.length)
-                    if (countryData.length !== 8) {
-                        countryData.push({
-                            name: country,
-                            uv: clientsInCountries[0][property]
-                        })
-                    }
-                }
-                for (const property in figures.beaconBlocks) {
-                    let figure = (property[0]).toUpperCase() + property.slice(1, property.length)
-                    beaconData.push({
-                        name: figure,
-                        uv: figures.beaconBlocks[property],
-                        scatterData: scatter
-                    })
-                }
-                for (const property in figures.beaconBlocksAggregations) {
-                    let figure = (property[0]).toUpperCase() + property.slice(1, property.length)
-                    aggregations.push({
-                        name: figure,
-                        uv: figures.beaconBlocksAggregations[property]
-                    })
-                }
-                for (const property in figures.averageLatency) {
-                    figures.averageLatency[property] = figures.averageLatency[property] / figures.quantityClients[property]
-                }
-                for (const property in figures.connections) {
-                    let figure = (property[0]).toUpperCase() + property.slice(1, property.length)
-                    connections.push({
-                        name: figure,
-                        uv: figures.connections[property] / figures.quantityClients[property]
-                    })
-                    figures.connections[property] = figures.connections[property] / figures.quantityClients[property]
-                }
-                for (const property in figures.disconnections) {
-                    let figure = (property[0]).toUpperCase() + property.slice(1, property.length)
-                    disconnections.push({
-                        name: figure,
-                        uv: figures.disconnections[property] / figures.quantityClients[property]
-                    })
-                    figures.disconnections[property] = figures.disconnections[property] / figures.quantityClients[property]
-                }
-                for (const property in figures.time) {
-                    let figure = (property[0]).toUpperCase() + property.slice(1, property.length)
-                    areas.push({
-                        name: figure,
-                        uv: figures.time[property]
-                    })
-                    figures.time[property] = figures.time[property] / figures.quantityClients[property]
-                }
-                for (const property in figures.quantityClients) {
-                    figures.quantityClientsPercentage[property] = parseFloat((figures.quantityClients[property] * (100 / totalClients)).toFixed(2))
-                    data.push({ name: property, value: parseFloat((figures.quantityClients[property] * (100 / totalClients)).toFixed(2)) })
-                }
-                for (const property in figures.beaconBlocks) {
-                    averageMessages.push({
-                        name: property,
-                        uv: (figures.beaconBlocks[property] / figures.quantityClients[property])
-                    })
-                    figures.beaconBlocksAverage[property] = figures.beaconBlocks[property] / figures.quantityClients[property]
-                }
-
-
+        metricsData.map((item) => {
+            scatter.push({ x: item.connected_time, y: item.total_messages, z: index })
+            totalClients += 1
+            if (!clientsInCountries[0].hasOwnProperty(item.country.toLowerCase())) {
+                clientsInCountries[0][item.country.toLowerCase()] = 1
+            } else {
+                clientsInCountries[0][item.country.toLowerCase()] += 1
+            }
+            client = item.client.toLowerCase()
+            if (figures.connections[client]) {
+                figures.connections[client] += item.connections
+                figures.disconnections[client] += item.disconnections
+                figures.time[client] += item.connected_time
+                figures.averageLatency[client] += item.latency
+                figures.beaconBlocks[client] += item.beacon_blocks
+                figures.quantityClients[client] += 1
+                figures.beaconBlocksAggregations[client] += item.beacon_aggregations
+            } else {
                 this.setState({
-                    info: figures,
-                    countryData: clientsInCountries,
-                    data: data,
-                    testData: countryData,
-                    beaconData: beaconData,
-                    scatterData: scatter,
-                    areaData: areas,
-                    aggregations: aggregations,
-                    connections: connections,
-                    disconnections: disconnections
+                    allClients: item.client
                 })
+                figures.connections[client] = item.connections || 0
+                figures.disconnections[client] = item.disconnections || 0
+                figures.time[client] = item.connected_time || 0
+                figures.averageLatency[client] = item.averageLatency || 0
+                figures.beaconBlocks[client] = item.beacon_blocks || 0
+                figures.quantityClients[client] = 1
+                figures.beaconBlocksAggregations[client] = item.beacon_aggregations || 0
+            }
+            index++
+        })
+        let country = ""
+        for (const property in clientsInCountries[0]) {
+            country = property[0].toUpperCase() + property.slice(1, property.length)
+            if (countryData.length !== 8) {
+                countryData.push({
+                    name: country,
+                    uv: clientsInCountries[0][property]
+                })
+            }
+        }
+        for (const property in figures.beaconBlocks) {
+            let figure = (property[0]).toUpperCase() + property.slice(1, property.length)
+            beaconData.push({
+                name: figure,
+                uv: figures.beaconBlocks[property],
+                scatterData: scatter
             })
+        }
+        for (const property in figures.beaconBlocksAggregations) {
+            let figure = (property[0]).toUpperCase() + property.slice(1, property.length)
+            aggregations.push({
+                name: figure,
+                uv: figures.beaconBlocksAggregations[property]
+            })
+        }
+        for (const property in figures.averageLatency) {
+            figures.averageLatency[property] = figures.averageLatency[property] / figures.quantityClients[property]
+        }
+        for (const property in figures.connections) {
+            let figure = (property[0]).toUpperCase() + property.slice(1, property.length)
+            connections.push({
+                name: figure,
+                uv: figures.connections[property] / figures.quantityClients[property]
+            })
+            figures.connections[property] = figures.connections[property] / figures.quantityClients[property]
+        }
+        for (const property in figures.disconnections) {
+            let figure = (property[0]).toUpperCase() + property.slice(1, property.length)
+            disconnections.push({
+                name: figure,
+                uv: figures.disconnections[property] / figures.quantityClients[property]
+            })
+            figures.disconnections[property] = figures.disconnections[property] / figures.quantityClients[property]
+        }
+        for (const property in figures.time) {
+            let figure = (property[0]).toUpperCase() + property.slice(1, property.length)
+            areas.push({
+                name: figure,
+                uv: figures.time[property]
+            })
+            figures.time[property] = figures.time[property] / figures.quantityClients[property]
+        }
+        for (const property in figures.quantityClients) {
+            figures.quantityClientsPercentage[property] = parseFloat((figures.quantityClients[property] * (100 / totalClients)).toFixed(2))
+            data.push({ name: property, value: parseFloat((figures.quantityClients[property] * (100 / totalClients)).toFixed(2)) })
+        }
+        for (const property in figures.beaconBlocks) {
+            averageMessages.push({
+                name: property,
+                uv: (figures.beaconBlocks[property] / figures.quantityClients[property])
+            })
+            figures.beaconBlocksAverage[property] = figures.beaconBlocks[property] / figures.quantityClients[property]
+        }
+
+
+        this.setState({
+            info: figures,
+            countryData: clientsInCountries,
+            data: data,
+            testData: countryData,
+            beaconData: beaconData,
+            scatterData: scatter,
+            areaData: areas,
+            aggregations: aggregations,
+            connections: connections,
+            disconnections: disconnections
+        })
     }
 
     openMenu = async () => {
